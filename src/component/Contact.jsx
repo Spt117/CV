b504c78acef1076867a7241f2eb02bc56acaadae
JB <theshitcoinert@gmail.com>
2023-01-22 22:41:58 +0100

    maj formulaire

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -15,40 +15,59 @@ export default function Contact() {
         console.log(form);
     }, [form]);
 
+    function eatFood() {
+        alert("Form has been submitted");
+    }
+
+    function test() {
+        console.log(myMessage);
+        eatFood();
+    }
+
     return (
         <div id="formulaire">
             <h1 id="contact-titre">Formulaire de contact</h1>
-            <div>
-                <div id="inputs">
-                    <input
-                        type="text"
-                        placeholder="Nom"
-                        onChange={(e) => (myMessage.name = e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Prénom"
-                        onChange={(e) => (myMessage.firstName = e.target.value)}
-                    />
-                    <input
-                        type="email"
-                        placeholder="e-mail"
-                        required="required"
-                        onChange={(e) => (myMessage.email = e.target.value)}
-                    />
+            <form method="POST" onSubmit={test}>
+                <div>
+                    <div id="inputs">
+                        <input
+                            type="text"
+                            required="required"
+                            placeholder="Nom"
+                            onChange={(e) => (myMessage.name = e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            required="required"
+                            placeholder="Prénom"
+                            onChange={(e) =>
+                                (myMessage.firstName = e.target.value)
+                            }
+                        />
+                        <input
+                            type="email"
+                            placeholder="e-mail*"
+                            required="required"
+                            onChange={(e) => (myMessage.email = e.target.value)}
+                        />
+                    </div>
+                    <div id="mymessage">
+                        <textarea
+                            required="required"
+                            name="message"
+                            id=""
+                            // cols="70"
+                            // rows="20"
+                            placeholder="Votre message..."
+                            onChange={(e) =>
+                                (myMessage.message = e.target.value)
+                            }
+                        ></textarea>
+                        <input type="submit" id="validation" />
+                    </div>
                 </div>
-                <div id="mymessage">
-                    <textarea
-                        name="message"
-                        id=""
-                        // cols="70"
-                        // rows="20"
-                        placeholder="Votre message..."
-                        onChange={(e) => (myMessage.message = e.target.value)}
-                    ></textarea>
-                    <button onClick={() => console.log(myMessage)}>Clic</button>
-                </div>
-            </div>
+            </form>
+            <button onClick={test}> Clic </button>
         </div>
     );
 }
