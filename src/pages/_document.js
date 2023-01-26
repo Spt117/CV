f35681db75d754e67bbf7a16f9e745298af38305
JB <theshitcoinert@gmail.com>
2023-01-26 20:42:29 +0100

    Menu avec anim

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
         <Html lang="en">
             <Head />
             <body>
-                {/* <Menu /> */}
+                <Menu />
                 <Main />
                 <NextScript />
             </body>
