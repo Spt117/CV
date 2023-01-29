0aa341405366c081b70420c69712e329bf27d099
jb <theshitcoinert@gmail.com>
2023-01-29 14:50:27 +0100

    Amélioration formulaire ajout site

--- src/component/AddSite.jsx
+++ src/component/AddSite.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 export default function AddSite() {
     const [site, setSite] = useState(null);
     const [mdp, setMdp] = useState("");
+    const [bool, setBool] = useState(false);
 
     async function sendSite() {
-        const bool = await checkMdp();
         if (bool) {
             const api = await fetch("/api/sites", {
                 method: "POST",
@@ -28,68 +28,79 @@ export default function AddSite() {
             body: JSON.stringify(mdp),
         });
         const res = await api.json();
-        return res;
+        setBool(res);
     }
 
     return (
         <div className="child1" id="myForm">
             <h1>Ajouter un site à mon Portfolio</h1>
-            <input
-                placeholder="Mot de passe"
-                onChange={(e) => setMdp(e.target.value)}
-            />
-            <form id="theForm2">
-                <input
-                    id="form-name"
-                    className="init"
-                    type="text"
-                    placeholder="Titre"
-                    onChange={(e) =>
-                        setSite({
-                            ...site,
-                            titre: e.target.value,
-                        })
-                    }
-                />
-                <input
-                    className="init"
-                    type="text"
-                    placeholder="Adresse du site"
-                    onChange={(e) =>
-                        setSite({
-                            ...site,
-                            url: e.target.value,
-                        })
-                    }
-                />
-                <input
-                    id="form-mail"
-                    className="init"
-                    type="text"
-                    placeholder="Description"
-                    onChange={(e) =>
-                        setSite({
-                            ...site,
-                            description: e.target.value,
-                        })
-                    }
-                />
-                <input
-                    id="form-subject"
-                    className="init"
-                    type="text"
-                    placeholder="Technos utilisées"
-                    onChange={(e) =>
-                        setSite({
-                            ...site,
-                            techno: e.target.value,
-                        })
-                    }
-                />
-            </form>
-            <button onClick={sendSite} className="btn" id="form-btn">
-                Ajouter
-            </button>
+            {!bool && (
+                <>
+                    <input
+                        placeholder="Mot de passe"
+                        onChange={(e) => setMdp(e.target.value)}
+                    />
+                    <button onClick={checkMdp} className="btn">
+                        Valider
+                    </button>
+                </>
+            )}
+            {bool && (
+                <>
+                    <form id="theForm2">
+                        <input
+                            id="form-name"
+                            className="init"
+                            type="text"
+                            placeholder="Titre"
+                            onChange={(e) =>
+                                setSite({
+                                    ...site,
+                                    titre: e.target.value,
+                                })
+                            }
+                        />
+                        <input
+                            className="init"
+                            type="text"
+                            placeholder="Adresse du site"
+                            onChange={(e) =>
+                                setSite({
+                                    ...site,
+                                    url: e.target.value,
+                                })
+                            }
+                        />
+                        <input
+                            id="form-mail"
+                            className="init"
+                            type="text"
+                            placeholder="Description"
+                            onChange={(e) =>
+                                setSite({
+                                    ...site,
+                                    description: e.target.value,
+                                })
+                            }
+                        />
+                        <input
+                            id="form-subject"
+                            className="init"
+                            type="text"
+                            placeholder="Technos utilisées"
+                            onChange={(e) =>
+                                setSite({
+                                    ...site,
+                                    techno: e.target.value,
+                                })
+                            }
+                        />
+                    </form>
+                    <button onClick={sendSite} className="btn" id="form-btn">
+                        Ajouter
+                    </button>
+                </>
+            )}
         </div>
     );
 }
