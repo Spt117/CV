959f8e42248acf76e2195e4f855d16bf5769e290
jb <theshitcoinert@gmail.com>
2023-01-29 13:53:20 +0100

    maj

--- src/pages/api/mdp.js
+++ src/pages/api/mdp.js
@@ -1,6 +1,5 @@
 export default (req, res) => {
     const Mdp = process.env.mdp;
     const body = req.body;
-    console.log(Mdp, body);
     body === Mdp ? res.status(201).json(true) : res.status(400).json(false);
 };
