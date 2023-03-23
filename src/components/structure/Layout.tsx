f9054c3b68e88795827eb9b1c10327f0a0e0b5b2
jb <theshitcoinert@gmail.com>
2023-03-23 22:07:44 +0100

    maj

--- src/components/structure/Layout.tsx
+++ src/components/structure/Layout.tsx
@@ -1,20 +1,19 @@
 import { fetchData } from "@/library/functions"
 import { Site } from "@/library/interfaces"
 import { mySites } from "@/library/redux"
-import { AnyAction, Dispatch } from "@reduxjs/toolkit"
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import Menu from "./Menu"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const dispatch: Dispatch<AnyAction> = useDispatch()
+    const dispatch = useDispatch()
 
     useEffect(() => {
         init()
     }, [])
 
-    async function init(): Promise<void> {
-        const sites: Site[] = (await fetchData("/api/sites")) as Site[]
+    async function init() {
+        const sites = (await fetchData("/api/sites")) as Site[]
         dispatch(mySites(sites))
     }
 
